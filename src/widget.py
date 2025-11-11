837b8883742b69ecad0f05c93fa76768eb75b3a1
SergGram <mazzzay73@gmail.com>
2025-11-11 12:40:05 +0400

    widget changes

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,23 @@
 from src.masks import get_mask_account, get_mask_card_number
-
+"""Импортируем функции для скрытия номера карты и счета из masks"""
 def mask_account_card(card_info: str) -> str:
-    parts = card_info.split()
-    number = parts[-1]
-    type_of_inf = parts[0]
+    """Функция принимает строку и выводит замаскированный номер"""
+    number = ""
+    type_of_inf = ""
+
+    for part in card_info:
+        if part.isdigit():
+            number += part
+        elif part.isalpha():
+            type_of_inf += part
 
     if type_of_inf == "счет" or type_of_inf == "Счет":
         masked_result = get_mask_account(number)
     else:
         masked_result = get_mask_card_number(number)
 
-    return f"{parts[0]} [**{masked_result}**]"
+    return f"{type_of_inf} [{masked_result}]"
+
+card_info = input("Please enter your number: ")
+result = mask_account_card(card_info)
+print(result)
