44ec2d616e2e041f599cf7ba3c8f9b3ac07945bf
SergGram <mazzzay73@gmail.com>
2025-11-25 22:37:27 +0400

    add test  function

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -11,3 +11,12 @@ def number():
 @pytest.mark.parametrize("result", ["4545 45** **** 4545"])
 def test_get_mask_card_number(number: str, result) -> str:
     assert get_mask_card_number(number) == result
+
+@pytest.fixture
+def account():
+    return "123456789"
+
+
+@pytest.mark.parametrize("masked", ["**6789"])
+def test_get_mask_account(account: str, masked) -> str:
+    assert get_mask_account(account) == masked
